7657864fef322726504f252633082afdb373b3ba
matheuzcapdevila <matheuz.lcc@gmail.com>
2022-01-17 18:29:16 -0300

    Exercicio novo

---
+++ ExemploPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExemploPOO.Models;
+
+namespace ExemploPOO {
+    class Program{
+        static void Main(string[] args){
+            Pessoa p1 = new Pessoa();
+            
+            p1.Nome = "Bob";
+            p1.Idade = 20;
+
+            p1.Apresentar(); 
+
+    }
+}
+}
